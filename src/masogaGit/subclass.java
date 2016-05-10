acdffa0cfac52f067dea9a593adfd00e946b05f4
Learner7pc <gift.masoga@gmail.com>
2016-05-10 11:58:08 +0200

    int num3 = 10; int sum = num1 + num2 + num3; int average = sum/3;

---
+++ src/masogaGit/subclass.java
@@ -0,0 +1,23 @@
+package masogaGit;
+
+public class subclass {
+	void maths(){ 
+			 
+		 		int num1 = 2; 
+				int num2 = 6; 
+				int num3 = 10;
+		 		int sum, average; 
+		 		 
+		 		sum = num1 + num2 + num3; 
+		 		average = sum/3; 
+		 		 
+		 		System.out.println("sum is "+sum + " Averagw is "+ average); 
+		 	} 
+		 
+		 
+		 	 
+		 		 
+		 	} 
+
+
+
